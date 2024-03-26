d3bed5e031aa13f8d0eed1344acff916f561c433
sanyauzhas <126069463+sanyauzhas@users.noreply.github.com>
2024-03-26 15:00:24 +0300

    Create main714.py

---
+++ main714.py
@@ -0,0 +1,55 @@
+import psycopg2
+from bs4 import BeautifulSoup
+import requests
+conn = psycopg2.connect(
+    host='localhost',
+    port='5432',
+    dbname='postgres',
+    user='postgres',
+    password='Q1w2e3r4'
+)
+
+cursor = conn.cursor()
+
+
+url = "https://sudar.su/catalog/jackets/" #ссылка на сайт
+
+def parse_insert():
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser') #считывание сайта
+
+    img_pictures = soup.find_all('img') #поиск картинок
+
+    for img in range(0,5):
+        picture_url = img_pictures[img]['src']
+        picture_id = img #ID картинок
+        with open(f'../images','wb') as f:
+            if 'https' not in picture_url: #корректировка ссылок
+                strg = 'https://sudar.su' + str(picture_url)
+                g = requests.get(strg)
+            else:
+                g = requests.get(picture_url)
+                strg = picture_url
+            with open(f'images/img'+str(picture_id)+'.jpg','wb') as d:
+                d.write(g.content) #загрузка в папку 
+        cursor.execute("INSERT INTO sudar_jackets (image_id, image_url) VALUES (%s, %s)", (picture_id, strg)) #загрузка в таблицу
+        conn.commit()
+    for img in range(6, len(img_pictures),2):
+        picture_url = img_pictures[img]['src']
+        picture_id = img  # ID картинок
+        with open(f'../images', 'wb') as f:
+            if 'https' not in picture_url:  # корректировка ссылок
+                strg = 'https://sudar.su' + str(picture_url)
+                g = requests.get(strg)
+            else:
+                g = requests.get(picture_url)
+                strg = picture_url
+            with open(f'images/img' + str(picture_id) + '.jpg', 'wb') as d:
+                d.write(g.content)  # загрузка в папку
+        cursor.execute("INSERT INTO sudar_jackets (image_id, image_url) VALUES (%s, %s)", (picture_id, strg))  # загрузка в таблицу
+        conn.commit()
+
+parse_insert()
+
+cursor.close()
+conn.close()
