38ec4e7723f04b9be7250a32ea9309ec3a6c9db8
sanyauzhas <126069463+sanyauzhas@users.noreply.github.com>
2024-03-26 14:10:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import psycopg2
 from bs4 import BeautifulSoup
 import requests
-conn = psycopg2.connect(
+conn = psycopg2.connect( #для связи с таблицей
     host='localhost',
     port= '5432',
     dbname='postgres',
@@ -12,29 +12,29 @@ conn = psycopg2.connect(
 cursor = conn.cursor()
 
 
-url = "https://sudar.su/catalog/jackets/"
+url = "https://sudar.su/catalog/jackets/" #ссылка на сайт
 
-def scrape_and_insert():
+def parse_insert():
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(response.text, 'html.parser') #считывание сайта
 
-    img_elements = soup.find_all('img')
+    img_pictures = soup.find_all('img') #поиск картинок
 
-    for img in range(len(img_elements)):
-        picture_url = img_elements[img]['src']
-        user_id = img+1
+    for img in range(len(img_pictures)):
+        picture_url = img_pictures[img]['src']
+        picture_id = img+1 #ID картинок
         with open(f'../images','wb') as f:
-            if 'https' not in picture_url:
+            if 'https' not in picture_url: #корректировка ссылок
                 strg = 'https://sudar.su' + str(picture_url)
                 g = requests.get(strg)
             else:
                 g = requests.get(picture_url)
-            with open(f'images/img'+str(user_id)+'.jpg','wb') as d:
-                d.write(g.content)
-        cursor.execute("INSERT INTO sudar_jackets (image_id, image_url) VALUES (%s, %s)", (user_id, picture_url))
+            with open(f'images/img'+str(picture_id)+'.jpg','wb') as d:
+                d.write(g.content) #загрузка в папку 
+        cursor.execute("INSERT INTO sudar_jackets (image_id, image_url) VALUES (%s, %s)", (picture_id, picture_url)) #загрузка в таблицу
         conn.commit()
 
-scrape_and_insert()
+parse_insert()
 
 cursor.close()
 conn.close()
