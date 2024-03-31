a8d8dc2404584b57887cb3ded4dd29ff1e7ee32c
sanyauzhas <126069463+sanyauzhas@users.noreply.github.com>
2024-03-31 21:34:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import psycopg2
 from bs4 import BeautifulSoup
 import requests
-conn = psycopg2.connect( #для связи с таблицей
+conn = psycopg2.connect(
     host='localhost',
-    port= '5432',
+    port='5432',
     dbname='postgres',
     user='postgres',
-    password='pass'
+    password='pass' #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 )
 
 cursor = conn.cursor()
@@ -17,22 +17,39 @@ url = "https://sudar.su/catalog/jackets/" #ссылка на сайт
 def parse_insert():
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser') #считывание сайта
-
     img_pictures = soup.find_all('img') #поиск картинок
-
-    for img in range(len(img_pictures)):
+    picture_id = 0
+    links = soup.find_all('a', class_='js-product-link-to-detail')
+    for img in range(0,5):
         picture_url = img_pictures[img]['src']
-        picture_id = img+1 #ID картинок
         with open(f'../images','wb') as f:
             if 'https' not in picture_url: #корректировка ссылок
                 strg = 'https://sudar.su' + str(picture_url)
                 g = requests.get(strg)
             else:
                 g = requests.get(picture_url)
+                strg = picture_url
             with open(f'images/img'+str(picture_id)+'.jpg','wb') as d:
-                d.write(g.content) #загрузка в папку 
-        cursor.execute("INSERT INTO sudar_jackets (image_id, image_url) VALUES (%s, %s)", (picture_id, picture_url)) #загрузка в таблицу
+                d.write(g.content) #загрузка в папку
+        cursor.execute("INSERT INTO sudar_jackets (image_id, image_url) VALUES (%s, %s)", (picture_id, strg)) #загрузка в таблицу
         conn.commit()
+        picture_id += 1
+    for img in range(6, len(img_pictures),2):
+        picture_url = img_pictures[img]['src']
+        with open(f'../images', 'wb') as f:
+            if 'https' not in picture_url:  # корректировка ссылок
+                strg = 'https://sudar.su' + str(picture_url)
+                g = requests.get(strg)
+            else:
+                g = requests.get(picture_url)
+                strg = picture_url
+            with open(f'images/img' + str(picture_id) + '.jpg', 'wb') as d:
+                d.write(g.content)  # загрузка в папку
+        str_url = 'https://sudar.su' + links[picture_id-5]['href']
+        cursor.execute("INSERT INTO sudar_jackets (image_id, image_url,product_url) VALUES (%s, %s, %s)",
+                       (picture_id, strg,str_url))  # загрузка в таблицу
+        conn.commit()
+        picture_id += 1
 
 parse_insert()
 
